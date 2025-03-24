aa964582c1e0a4e44a98467ffba2ffb0235b999b
christify30 <39292608+christify30@users.noreply.github.com>
2025-03-24 11:59:02 +0100

    refactor

--- submission/03.sh
+++ submission/03.sh
@@ -1,9 +1,6 @@
 # Created a SegWit address.
 SegwitWalletAddress=$(bitcoin-cli -regtest -rpcwallet="btrustwallet" getnewaddress "" "bech32")
-echo "the created walet"
-echo "$SegwitWalletAddress"
-
 # Add funds to the address.
-bitcoin-cli -regtest generatetoaddress 101 "$SegwitWalletAddress"
+AddFunds=$(bitcoin-cli -regtest generatetoaddress 101 "$SegwitWalletAddress")
 # Return only the Address
 echo "$SegwitWalletAddress"
