f349908b21b4a3649c405ed9a73a44b1ee938065
christify30 <39292608+christify30@users.noreply.github.com>
2025-03-24 11:52:52 +0100

    testing

--- submission/03.sh
+++ submission/03.sh
@@ -1,5 +1,12 @@
 # Created a SegWit address.
 SegwitWalletAddress=$(bitcoin-cli -regtest -rpcwallet="btrustwallet" getnewaddress "" "bech32")
+
+if [ -z "$SegwitWalletAddress" ]; then
+  echo "Error: No address was generated. Check if the wallet exists and supports SegWit."
+  exit 1
+fi
+
+
 # Add funds to the address.
 bitcoin-cli -regtest generatetoaddress 101 "$SegwitWalletAddress"
 # Return only the Address
