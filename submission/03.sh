a9b855a081ac246e0cb4709d8fcbd5fbbac109de
christify30 <39292608+christify30@users.noreply.github.com>
2025-03-25 22:15:37 +0100

    add funds

--- submission/03.sh
+++ submission/03.sh
@@ -2,6 +2,6 @@
 SegwitWalletAddress=$(bitcoin-cli -regtest -rpcwallet="btrustwallet" getnewaddress "" "bech32")
 # Add funds to the address.
 #AddFunds=$(bitcoin-cli -regtest generatetoaddress 101 "$SegwitWalletAddress")
-AddFunds=$(bitcoin-cli -regtest sendtoaddress "$SegwitWalletAddress" 4.54001)
+AddFunds=$(bitcoin-cli -regtest sendtoaddress "$SegwitWalletAddress" 4.54001 true)
 # Return only the Address
 echo "$SegwitWalletAddress"
