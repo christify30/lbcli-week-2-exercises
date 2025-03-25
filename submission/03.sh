94fbbdd596f8f9c7351d61861dc456e4440c7bfb
christify30 <39292608+christify30@users.noreply.github.com>
2025-03-25 22:05:32 +0100

    use send to address

--- submission/03.sh
+++ submission/03.sh
@@ -1,6 +1,7 @@
 # Created a SegWit address.
 SegwitWalletAddress=$(bitcoin-cli -regtest -rpcwallet="btrustwallet" getnewaddress "" "bech32")
 # Add funds to the address.
-AddFunds=$(bitcoin-cli -regtest generatetoaddress 101 "$SegwitWalletAddress")
+#AddFunds=$(bitcoin-cli -regtest generatetoaddress 101 "$SegwitWalletAddress")
+AddFunds=$(bitcoin-cli -regtest sendtoaddress "$SegwitWalletAddress" 4.54001)
 # Return only the Address
 echo "$SegwitWalletAddress"
